aa6a264bd2aef73b65e43fb41266fdca431a9aa7
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-25 18:30:10 +0530

    practice

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -1,33 +1,52 @@
-    //     getname();
-    //   console.log(x);
-    //   console.log(getname);
-    //   var x = 7;
-    //   function getname() {
-    //     console.log("Iam new to javascript");
-    //   }
-    //   var x=10;
-    //   function b(){
-    //     var a =10;
-    //   }
-    //   console.log(x);
-    //   console.log(b);
-    //   console.log(a);
-    // var a;
-    // console.log(a)
-    // if (a===undefined){
-    //     console.log("a is undifined")
-    // }
-    // else{
-    //     console.log("a is not undifined")
-    // }
-    // var a=10;
-    // if (a===undefined){
-    //     console.log("a is undefined")
-    // }
-    // else{
-    //     console.log("a is defined")
-    // }
-    // var a="10"
-    // console.log(a)
-    // var a=10
-    // console.log(a)
+//     getname();
+//   console.log(x);
+//   console.log(getname);
+//   var x = 7;
+//   function getname() {
+//     console.log("Iam new to javascript");
+//   }
+//   var x=10;
+//   function b(){
+//     var a =10;
+//   }
+//   console.log(x);
+//   console.log(b);
+//   console.log(a);
+// var a;
+// console.log(a)
+// if (a===undefined){
+//     console.log("a is undifined")
+// }
+// else{
+//     console.log("a is not undifined")
+// }
+// var a=10;
+// if (a===undefined){
+//     console.log("a is undefined")
+// }
+// else{
+//     console.log("a is defined")
+// }
+// var a="10"
+// console.log(a)
+// var a=10
+// console.log(a)
+// var b = 10;
+// function a() {
+//   var c = 20;
+//   console.log(b);
+//   d();
+//   function d() {
+//     var e = 30;
+//     console.log(c);
+//     f();
+//     function f() {
+//       var g = 40;
+//       console.log(b);
+//       console.log(c);
+//       console.log(e);
+//       console.log(g);
+//     }
+//   }
+// }
+// a();
