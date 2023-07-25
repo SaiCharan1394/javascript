827096a17a857b0f08c0e5ca853ac6ada6b4c444
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-25 14:37:13 +0530

    practice

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -1,8 +1,14 @@
-        getname();
-      console.log(x);
-      console.log(getname);
-      var x = 7;
-      function getname() {
-        console.log("Iam new to javascript");
-      }
-      
+    //     getname();
+    //   console.log(x);
+    //   console.log(getname);
+    //   var x = 7;
+    //   function getname() {
+    //     console.log("Iam new to javascript");
+    //   }
+    //   var x=10;
+    //   function b(){
+    //     var a =10;
+    //   }
+    //   console.log(x);
+    //   console.log(b);
+    //   console.log(a);
