421d5860113921faa0bd372065b9f49826fda783
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-27 14:27:44 +0530

    changes

--- Javascript Practice/index.js
+++ Javascript Practice/index.js
@@ -52,14 +52,24 @@
 // a();
 //block scope
 
-var a=100;
-{
-    var a=20
-    let b=100;
-    const c=100;
-    console.log(a);
-    console.log(b);
-    console.log(c);
-
+// var a=100;
+// {
+//     var a=20
+//     let b=100;
+//     const c=100;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+function x(){
+    var a=1;
+    setTimeout (function y(){
+        var b=2;
+        setTimeout (function z(){
+            console.log(b)
+        },6000)
+        console.log(a)
+    },5000)
+    console.log("saicharan")
 }
-
+x();
