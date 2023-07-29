5047ecbbfa892a6df973ef841ad292aaa458bffb
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-29 13:03:48 +0530

    changes

---
+++ ControlFlow/index.js
@@ -0,0 +1,85 @@
+// If else
+
+// let hour = 10;
+// if (hour >= 6 && hour < 12) {
+//   console.log("Good Morning");
+// } else if (hour >= 12 && hour < 18) {
+//   console.log("Good Afternoon");
+// } else {
+//   console.log("Good Evening");
+// }
+
+
+
+// Switch Case
+
+// let role;
+// switch (role){
+//     case 'guest':
+//         console.log('Guest User');
+//         break;
+//     case 'moderator':
+//         console.log('Moderator User');
+//         break;
+//     default:
+//         console.log('Unknown User');
+// }
+
+
+// For loop
+
+// for (let i=1;i<=5;i++){
+//     if (i/2 === 0){
+//         ans=(i/2)
+//         console.log(ans);
+//     }
+//     else{
+//         console.log(i,' is not divided by 2')
+//     }
+// }
+
+
+// While Loop
+
+//  let i=1;
+//  while (i<=5){
+//     if (i%2 !==0){
+//         console.log(i)
+//     }
+//     i++
+//  }
+
+
+// Do While loop
+
+//   let i=10;
+//   do{
+//     if (i%2 === 0){
+//         console.log(i);
+//     }
+//     i++;
+//   } while(i<=5)
+
+
+// For In
+
+// let colour=['red','green','yellow'];
+// for (let index in colour){
+//     console.log(index,colour[index]);
+// }
+// let person={
+//     name:'sai',
+//     age:30
+// }
+// for (let value in person ){
+//     console.log(value,person[value]);
+// }
+
+
+// For Of
+
+// let person=['red','yellow','blue','green']
+
+// for (let persons of person){
+//     console.log(persons)
+// }
