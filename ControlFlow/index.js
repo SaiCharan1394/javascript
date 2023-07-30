b5983a94ccadda1699959930cbd7dffdb2a7c95f
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-30 18:35:56 +0530

    changes

--- ControlFlow/index.js
+++ ControlFlow/index.js
@@ -194,11 +194,3 @@
 // practice
 
 
-function fizzbuzz(input){
-    if (typeof(input) !=='number') return NaN 
-    else if (input%3===0 && input%5===0)return 'FizzBuzz'
-    else if (input%3===0) return 'Fizz'
-    else if (input%5===0) return 'Buzz'
-    else return input
-}
-console.log(fizzbuzz("sai"));
