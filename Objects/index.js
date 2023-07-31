084be3ef63da0475de79efd31469e4e982610b71
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-31 10:13:00 +0530

    changes

--- Objects/index.js
+++ Objects/index.js
@@ -1,25 +1,29 @@
 
-function createCircle(radius){
-    return{
-        radius,
-        draw(){
-            console.log('draw');
-        }
-    }
-}
-createCircle.color='yellow';
-console.log(createCircle)
-
-
-const circle={
-    ridus:1,
-    draw(){
-        console.log('draw')
-    }
-};
-// const another={};
-// for (let key in circle){
-//     another[key]=circle[key];
+// function createCircle(radius){
+//     return{
+//         radius,
+//         draw(){
+//             console.log('draw');
+//         }
+//     }
 // }
-const another=Object.assign({},circle);
-console.log(another)
+// createCircle.color='yellow';
+// console.log(createCircle)
+
+
+// const circle={
+//     ridus:1,
+//     draw(){
+//         console.log('draw')
+//     }
+// };
+// // const another={};
+// // for (let key in circle){
+// //     another[key]=circle[key];
+// // }
+// const another=Object.assign({},circle);
+// console.log(another)
+
+const message='This ia my first message'
+console.log(message.length)
+console.log(message.replace('first','second'))
