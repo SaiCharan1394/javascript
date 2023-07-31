dd7a6edb273d6a7098ae5e8438bb9e76ed510fd5
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-07-31 10:47:57 +0530

    changes

--- Objects/index.js
+++ Objects/index.js
@@ -24,6 +24,50 @@
 // const another=Object.assign({},circle);
 // console.log(another)
 
-const message='This ia my first message'
-console.log(message.length)
-console.log(message.replace('first','second'))
+// const message='This is my first message'
+// console.log(message.length)
+// console.log(message.replace('first','second'))
+// console.log(message.toUpperCase())
+// console.log(message.toLowerCase())
+// let age=20;
+// const another=`i am ${age}
+// charan is my 
+// friend.`;
+// console.log(another)
+
+
+//exercise
+
+
+//  const showAddress={
+//     street:'gandallapalli',
+//     city:'pileru',
+//     zipcode:517214
+
+// };
+// for (let keys in showAddress)
+// console.log(keys,':',showAddress[keys])
+// // console.log(showAddress[keys])
+
+
+// factory function
+
+// function createAddress(street,city,zipcode){
+//     return{
+//         street,
+//         city,
+//         zipcode
+//     }
+// }
+// console.log(createAddress('a','pileru',517214));
+
+
+// // constrctor function
+
+// function Address(street,city,zipcode){
+//     this.street=street;
+//     this.city=city;
+//     this.zipcode=zipcode;
+// }
+// let address=new Address('a','pileru',517214)
+// console.log(address)
